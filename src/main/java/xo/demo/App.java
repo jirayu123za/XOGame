dc8475be16840558dc919010ea9fd92c373a8bf6
jirayu <jirayu004za@gmail.com>
2023-05-22 22:29:06 +0700

    rename class to easy understand

--- src/main/java/xo/demo/App.java
+++ src/main/java/xo/demo/App.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class xo {
-
+public class App{
 	public static void main(String[] args) {
-		SpringApplication.run(xo.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
