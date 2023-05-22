3ab2ccd82a23765e6c84ab38904312bac821774d
jirayu <jirayu004za@gmail.com>
2023-05-22 19:20:14 +0700

    Initial commit

---
+++ src/main/java/xo/demo/xo.java
@@ -0,0 +1,13 @@
+package xo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class xo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(xo.class, args);
+	}
+
+}
---
+++ src/test/java/xo/demo/xoTests.java
@@ -0,0 +1,13 @@
+package xo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class xoTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
