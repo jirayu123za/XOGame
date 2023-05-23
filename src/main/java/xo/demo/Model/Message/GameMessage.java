7a29f3e8c27b31d5c9e0f8487d01f82fe6b3e366
jirayu <jirayu004za@gmail.com>
2023-05-23 20:09:37 +0700

    implement class and declare variables with @Setter @Getter

--- src/main/java/xo/demo/Model/Message/GameMessage.java
+++ src/main/java/xo/demo/Model/Message/GameMessage.java
@@ -1,4 +1,29 @@
 package xo.demo.Model.Message;
+import lombok.*;
+import xo.demo.Model.Game;
+import xo.demo.State.GameState;
 
-public class GameMessage {
+@Setter @Getter
+public class GameMessage{
+    private String type;
+    private String player1, player2;
+    private String gameID;
+    private String content;
+    private String winner;
+    private String turn;
+    private String sender;
+    private String[][] board;
+    private int move;
+    private GameState gameState;
+
+    public GameMessage(){}
+    public GameMessage(Game game){
+        this.gameID = game.getID();
+        this.player1 = game.getPlayer1();
+        this.player2 = game.getPlayer2();
+        this.winner = game.getWinner();
+        this.turn = game.getTurn();
+        this.board = game.getBoard();
+        this.gameState = game.getGameState();
+    }
 }
