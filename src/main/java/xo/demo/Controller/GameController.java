7da26886965fcd43300aae51d5d617950d47f683
jirayu <jirayu004za@gmail.com>
2023-05-23 18:42:54 +0700

    implement class to complete, with Renders the XO game page with an empty board and Controller class for handling HTTP requests and rendering the XO game page.

--- src/main/java/xo/demo/Controller/GameController.java
+++ src/main/java/xo/demo/Controller/GameController.java
@@ -1,5 +1,28 @@
 package xo.demo.Controller;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.*;
+import java.util.*;
 
+// Controller class for handling HTTP requests and rendering the XO game page.
+@Controller
+@RequestMapping("/")
 public class GameController{
+    /**
+     * Renders the XO game page with an empty board.
+     * @return the model and view for the XO game page
+     */
+    @GetMapping
+    public ModelAndView position(){
+        return game();
+    }
 
+    @RequestMapping("/position")
+    public ModelAndView game(){
+        ModelAndView modelAndView = new ModelAndView("position");
+        String[][] board = new String[3][3];
+        Arrays.stream(board).forEach(row -> Arrays.fill(row, " "));
+        modelAndView.addObject("board", board);
+        return modelAndView;
+    }
 }
