a77bf604fc3b259b93fff7646d3927fd3b2b1678
jirayu <jirayu004za@gmail.com>
2023-05-23 18:46:06 +0700

    implement WebSocketConfiguration to complete

--- src/main/java/xo/demo/Configuration/WebSocketConfiguration.java
+++ src/main/java/xo/demo/Configuration/WebSocketConfiguration.java
@@ -1,5 +1,22 @@
 package xo.demo.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
-public class WebSocketConfiguration{
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer{
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        // Register Stomp endpoints mapping to "/ws" and enable SockJS fallback options
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    }
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry){
+        // Register Stomp endpoints mapping to "/ws" and enable SockJS fallback options
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/queue", "topic", "user");
+        registry.setUserDestinationPrefix("/user");
+    }
 }
