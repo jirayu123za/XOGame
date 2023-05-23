892ad94264c081a4ee2fdcb08cb6729e866fed66
jirayu <jirayu004za@gmail.com>
2023-05-23 20:06:27 +0700

    add declare variable type, player, gameID, content

--- src/main/java/xo/demo/Model/Message/PlayerMessage.java
+++ src/main/java/xo/demo/Model/Message/PlayerMessage.java
@@ -1,4 +1,11 @@
 package xo.demo.Model.Message;
+import lombok.*;
 
+@Setter
+@Getter
 public class PlayerMessage{
+    private String type;
+    private String player;
+    private String gameID;
+    private String content;
 }
