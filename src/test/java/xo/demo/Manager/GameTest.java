ae9476137e2e3c28b27ff1896389f825c2ce0fe0
jirayu <jirayu004za@gmail.com>
2023-05-24 21:25:48 +0700

    implement all functions to complete and correct

--- src/test/java/xo/demo/Manager/GameTest.java
+++ src/test/java/xo/demo/Manager/GameTest.java
@@ -22,54 +22,92 @@ public class GameTest{
 
     @Test
     void testJoinGame_second(){
-        // incomplete
+        GameManager manager = new GameManager();
+        manager.joinGame("Player1");
+        Game game = manager.joinGame("Player2");
+        Assertions.assertEquals("Player1", game.getPlayer1());
+        Assertions.assertEquals("Player2", game.getPlayer2());
+        Assertions.assertEquals(GameState.player1Turn, game.getGameState());
     }
 
     @Test
     void testJoinGame_alreadyInGame(){
-        // incomplete
+        GameManager manager = new GameManager();
+        Game game = manager.joinGame("Player1");
+        Game game2 = manager.joinGame("Player1");
+        Assertions.assertEquals(game, game2);
+        Assertions.assertEquals("Player1", game2.getPlayer1());
+        Assertions.assertNull(game2.getPlayer2());
+        Assertions.assertEquals(GameState.waitingPlayer, game2.getGameState());
     }
 
     // leaveGame()
     @Test
     void testLeaveGame_first(){
-        // incomplete
+        GameManager manager = new GameManager();
+        Game game = manager.joinGame("Player1");
+        manager.leaveGame("Player1");
+        Assertions.assertNull(manager.getGame(game.getID()));
     }
 
     @Test
     void testLeaveGame_second(){
-        // incomplete
+        GameManager manager = new GameManager();
+        Game game = manager.joinGame("Player1");
+        manager.joinGame("Player2");
+        manager.leaveGame("Player2");
+        Assertions.assertNotNull(manager.getGame(game.getID()));
+        Assertions.assertEquals("Player1", manager.getGame(game.getID()).getPlayer1());
+        Assertions.assertNull(manager.getGame(game.getID()).getPlayer2());
     }
 
     @Test
     void testLeaveGame_waitingPlayer(){
-        // incomplete
+        GameManager manager = new GameManager();
+        manager.joinGame("Player1");
+        manager.leaveGame("Player1");
+        Assertions.assertTrue(manager.waitingPlayers.isEmpty());
     }
 
     @Test
     void testLeaveGame_nonexistentPlayer(){
-        // incomplete
+        GameManager manager = new GameManager();
+        Game game = manager.joinGame("Player1");
+        manager.leaveGame("Player2");
+        Assertions.assertNotNull(manager.getGame(game.getID()));
+        Assertions.assertEquals("Player1", manager.getGame(game.getID()).getPlayer1());
+        Assertions.assertNull(manager.getGame(game.getID()).getPlayer2());
     }
 
     // removeGame()
     @Test
     void testRemoveGame_exists(){
-        // incomplete
+        GameManager manager = new GameManager();
+        Game game = manager.joinGame("Player1");
+        manager.removeGame(game.getID());
+        Assertions.assertNull(manager.getGame(game.getID()));
     }
 
     @Test
     void testRemoveGame_doesNotExist(){
-        // incomplete
+        GameManager manager = new GameManager();
+        Game game = manager.joinGame("Player1");
+        manager.removeGame("Invalid game's ID");
+        Assertions.assertNotNull(manager.getGame(game.getID()));
     }
 
     // getGame()
     @Test
     void testGetGame_exists(){
-        // incomplete
+        GameManager manager = new GameManager();
+        Game game = manager.joinGame("Player1");
+        Assertions.assertEquals(game, manager.getGame(game.getID()));
     }
 
     @Test
     void testGetGame_doesNotExist() {
-        // incomplete
+        GameManager manager = new GameManager();
+        Game game = manager.joinGame("Player1");
+        Assertions.assertNull(manager.getGame("Invalid game's ID"));
     }
 }
